fca375c2cef85b7882e711edc0732af7b8e87d72
aaronsonss <67204619+aaronsonss@users.noreply.github.com>
2026-02-25 13:45:56 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/'],
       },
     ],
-    sitemap: 'https://www.quant-ent.com/sitemap.xml',
+   sitemap: 'https://quant-ent.com/sitemap.xml',
   };
 }
